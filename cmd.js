651482d90602baae6e295f05b6083736db12adc7
Hugh Perkins <hughperkins@gmail.com>
2015-02-25 00:25:42 +0700

    add jobs.json storage

--- cmd.js
+++ cmd.js
@@ -41,6 +41,9 @@ prompt.get(schema, function( err, result ) {
     }
 });
             
+if( fs.existsSync( __dirname + '/jobs.json' ) ) {
+    jobs = JSON.parse(fs.readFileSync(__dirname + '/jobs.json', 'utf8'));
+}
 
 function serveStatic( filepath, type, response ) {
     var filepath = filepath;
@@ -56,6 +59,17 @@ function serveStatic( filepath, type, response ) {
     });
 }
 
+function jobsToSimplify( jobs ) {
+    var jobssimple = [];
+    var jobIds = Object.keys( jobs );
+    for( var i = 0; i < jobIds.length; i++ ) {
+        var job = jobs[i];
+        var jobSimple = { 'id': job.id, 'cmd': job.cmd, 'args': job.args, 'done': job.done };
+        jobssimple[i] = jobSimple;
+    }
+    return jobssimple;
+}
+
 function serveStaticJs( filepath, response ) {
     serveStatic( filepath, 'text/javascript', response );
 }
@@ -64,115 +78,115 @@ function serveStaticHtml( filepath, response ) {
     serveStatic( filepath, 'text/html', response );
 }
 
-function handleRequest( queryDic, response ) {
-    var cmd = queryDic.cmd;
-    var dir = queryDic.dir;
-    var args = queryDic.args;
-    if( typeof dir == 'undefined' ) {
-        dir = '.';
-    }
-    if( typeof cmd == 'undefined' ) {
-        cmd = '';
-    }
-    console.log(cmd);
-    var check = queryDic.check;
-    var ourcheck = md5.md5(password + '||' + dir + '||' + cmd );
-    response.writeHead(200, {'Content-type': 'text/html; charset=utf-8' });
-    console.log(check);
-    console.log(ourcheck);
-//    if( typeof args == 'undefined' ) {
-//        args = '';
+//function handleRequest( queryDic, response ) {
+//    var cmd = queryDic.cmd;
+//    var dir = queryDic.dir;
+//    var args = queryDic.args;
+//    if( typeof dir == 'undefined' ) {
+//        dir = '.';
 //    }
-    response.write('<html>');
-    response.write('<head>');
-    response.write('<script src="md5.min.js"></script>');
-    response.write('<script type="text/javascript">');
-    response.write('function go() {' );
-//        response.write('   var passvalue = window.prompt("password","");');
-    response.write('   var passvalue = document.getElementById("pass").value;');
-//        response.write('   alert( passvalue );' );
-    response.write('   var dirvalue = document.getElementById("dir").value;');
-    response.write('   var cmdvalue = document.getElementById("cmd").value;');
-//    response.write('   var argsvalue = document.getElementById("args").value;');
-    response.write('   var checktarget = passvalue + "||" + dirvalue + "||" + cmdvalue;');
-    response.write('   var checkvalue = md5(checktarget);');
-    response.write('   check.value = checkvalue;');
-    response.write('   document.forms["myform"].submit();');
-    response.write('}');
-    response.write('</script>');
-    response.write('</head>');
-    response.write('<body>');
-//    for( var i = 0; i < jobs.length; i++ ) {
-//        response.write( jobs[i].cmd + '<form name="results"' + i + '" method="post" action="showresults">');
-//        response.write( '<input type="hidden" name=
-//        showresults?jobid=' + i + '</a><br/>' );
-//        response.write( '</form>' );
+//    if( typeof cmd == 'undefined' ) {
+//        cmd = '';
 //    }
-    response.write('<form name="myform" method="post" action="/">');
-    response.write('check: <input type="hidden" id="check" name="check" value=""></input>');
-    response.write('dir: <input type="text" size="60" id="dir" name="dir" value="' + dir + '" onKeydown="Javascript: if (event.keyCode==13) go();"></input><br/>');
-    response.write('cmd: <input size="140" type="text" id="cmd" name="cmd" value="' + cmd + '" onKeydown="Javascript: if (event.keyCode==13) go();"></input><br/>');
-//    response.write('args: <input type="text" id="args" name="args" value="' + args + '" onKeydown="Javascript: if (event.keyCode==13) go();"></input>');
-    if( isSsl ) {
-        response.write('pass: <input type="password" id="pass" name="pass" value="' + queryDic.pass + '" onKeydown="Javascript: if (event.keyCode==13) go();"></input>');
-    }
-    response.write('</form>');
-    if( !isSsl ) {
-        response.write('pass: <input type="password" id="pass" name="pass"  onKeydown="Javascript: if (event.keyCode==13) go();"></input>');
-    }
-    response.write('<input type="button" value="Submit" onclick="go()" /><br />\n');
+//    console.log(cmd);
+//    var check = queryDic.check;
+//    var ourcheck = md5.md5(password + '||' + dir + '||' + cmd );
+//    response.writeHead(200, {'Content-type': 'text/html; charset=utf-8' });
+//    console.log(check);
+//    console.log(ourcheck);
+////    if( typeof args == 'undefined' ) {
+////        args = '';
+////    }
+//    response.write('<html>');
+//    response.write('<head>');
+//    response.write('<script src="md5.min.js"></script>');
+//    response.write('<script type="text/javascript">');
+//    response.write('function go() {' );
+////        response.write('   var passvalue = window.prompt("password","");');
+//    response.write('   var passvalue = document.getElementById("pass").value;');
+////        response.write('   alert( passvalue );' );
+//    response.write('   var dirvalue = document.getElementById("dir").value;');
+//    response.write('   var cmdvalue = document.getElementById("cmd").value;');
+////    response.write('   var argsvalue = document.getElementById("args").value;');
+//    response.write('   var checktarget = passvalue + "||" + dirvalue + "||" + cmdvalue;');
+//    response.write('   var checkvalue = md5(checktarget);');
+//    response.write('   check.value = checkvalue;');
+//    response.write('   document.forms["myform"].submit();');
+//    response.write('}');
+//    response.write('</script>');
+//    response.write('</head>');
+//    response.write('<body>');
+////    for( var i = 0; i < jobs.length; i++ ) {
+////        response.write( jobs[i].cmd + '<form name="results"' + i + '" method="post" action="showresults">');
+////        response.write( '<input type="hidden" name=
+////        showresults?jobid=' + i + '</a><br/>' );
+////        response.write( '</form>' );
+////    }
+//    response.write('<form name="myform" method="post" action="/">');
+//    response.write('check: <input type="hidden" id="check" name="check" value=""></input>');
+//    response.write('dir: <input type="text" size="60" id="dir" name="dir" value="' + dir + '" onKeydown="Javascript: if (event.keyCode==13) go();"></input><br/>');
+//    response.write('cmd: <input size="140" type="text" id="cmd" name="cmd" value="' + cmd + '" onKeydown="Javascript: if (event.keyCode==13) go();"></input><br/>');
+////    response.write('args: <input type="text" id="args" name="args" value="' + args + '" onKeydown="Javascript: if (event.keyCode==13) go();"></input>');
+//    if( isSsl ) {
+//        response.write('pass: <input type="password" id="pass" name="pass" value="' + queryDic.pass + '" onKeydown="Javascript: if (event.keyCode==13) go();"></input>');
+//    }
+//    response.write('</form>');
+//    if( !isSsl ) {
+//        response.write('pass: <input type="password" id="pass" name="pass"  onKeydown="Javascript: if (event.keyCode==13) go();"></input>');
+//    }
+//    response.write('<input type="button" value="Submit" onclick="go()" /><br />\n');
 
-    if( typeof cmd != 'undefined' ) {
-        if( check != ourcheck ) {
-            response.write('password invalid');
-            response.end();
-        } else {
-            var job = {};
-            job['cmd'] = cmd;
-            jobs[nextJob] = job;
-            nextJob++;
-            var splitcmd = cmd.split(' ' );
-            var cmd1 = splitcmd[0];
-            response.write('cmd: ' + cmd + '<br />\n' );
-            options = {cwd: dir }
-            if( splitcmd.length > 1 ) {
-                var args = splitcmd.slice(1);
-                var cmdobj = spawn( cmd1, args, options );
-            } else {
-                var cmdobj = spawn( cmd1, options );
-            }
-            job.cmdobj = cmdobj;
-            job.cmd1 = cmd1;
-            job.args = args;
-            job.results = '';
-            cmdobj.on('exit', function(code) {
-                response.write('done, code: ' + code );
-                response.end();
-                job.finished = true;
-            });
-            cmdobj.on('error', function(code) {
-                response.write('error, code: ' + code );
-                response.end();
-                job.finished = true;
-            });
-            cmdobj.stdout.on('data', function(data) {
-                var thisresult = String(data).split('\n').join('<br/>\n' ).replace(/ /g, '&nbsp;' );
-                job.results += thisresult;
-                response.write( thisresult );
-            });
-            cmdobj.stderr.on('data', function(data) {
-                response.write( 'stderr: ' + String(data).split('\n').join('<br/>\n' ).replace(/ /g, '&nbsp;' ) );
-            });
-            cmdobj.on('close', function (code) {
-                response.write('child process exited with code ' + code);
-                job.finished = true;
-            });
-        }
-    } else {
-        response.write('</body><html>');
-        response.end();
-    }
-}
+//    if( typeof cmd != 'undefined' ) {
+//        if( check != ourcheck ) {
+//            response.write('password invalid');
+//            response.end();
+//        } else {
+//            var job = {};
+//            job['cmd'] = cmd;
+//            jobs[nextJob] = job;
+//            nextJob++;
+//            var splitcmd = cmd.split(' ' );
+//            var cmd1 = splitcmd[0];
+//            response.write('cmd: ' + cmd + '<br />\n' );
+//            options = {cwd: dir }
+//            if( splitcmd.length > 1 ) {
+//                var args = splitcmd.slice(1);
+//                var cmdobj = spawn( cmd1, args, options );
+//            } else {
+//                var cmdobj = spawn( cmd1, options );
+//            }
+//            job.cmdobj = cmdobj;
+//            job.cmd1 = cmd1;
+//            job.args = args;
+//            job.results = '';
+//            cmdobj.on('exit', function(code) {
+//                response.write('done, code: ' + code );
+//                response.end();
+//                job.finished = true;
+//            });
+//            cmdobj.on('error', function(code) {
+//                response.write('error, code: ' + code );
+//                response.end();
+//                job.finished = true;
+//            });
+//            cmdobj.stdout.on('data', function(data) {
+//                var thisresult = String(data).split('\n').join('<br/>\n' ).replace(/ /g, '&nbsp;' );
+//                job.results += thisresult;
+//                response.write( thisresult );
+//            });
+//            cmdobj.stderr.on('data', function(data) {
+//                response.write( 'stderr: ' + String(data).split('\n').join('<br/>\n' ).replace(/ /g, '&nbsp;' ) );
+//            });
+//            cmdobj.on('close', function (code) {
+//                response.write('child process exited with code ' + code);
+//                job.finished = true;
+//            });
+//        }
+//    } else {
+//        response.write('</body><html>');
+//        response.end();
+//    }
+//}
 
 function getQueryDic( request, callback ) {
     if( request.method == 'POST' ) {
@@ -200,42 +214,57 @@ function getQueryDic( request, callback ) {
     }
 }
 
+function writeJobs() {
+    var jobssimple = [];
+    var jobIds = Object.keys( jobs );
+    for( var i = 0; i < jobIds.length; i++ ) {
+        var job = jobs[i];
+        var jobSimple = { 'id': job.id, 'cmd': job.cmd, 'args': job.args, 'done': true, 'results': job.results };
+        jobssimple[i] = jobSimple;
+    }
+    fs.writeFileSync(__dirname + '/tempjobs.json', JSON.stringify(jobssimple), 'utf8' );
+    if( fs.existsSync(__dirname + '/jobs.json' ) ) {
+        //fs.unlinkSync(__dirname + '/jobs.json' );
+    }
+    fs.renameSync( __dirname + '/tempjobs.json', __dirname + '/jobs.json' );
+}
+
 function serverFunction( request, response ) {
     var path = url.parse( request.url ).pathname;
     console.log('received request: ' + request.url );
-    if( path == '/' ) {
-        if( request.method == 'POST' ) {
-            var queryData = '';
-            var queryDic = '';
-            request.on('data', function(data ) {
-                queryData += data;
-                if( queryData.length > 1e6 ) {
-                    queryData = '';
-                    response.writeHead(413, {'Content-Type': 'text/plain'}).end();
-                    request.connection.destroy();
-                }
-            });
-            request.on('end', function() {
-                queryDic = querystring.parse(queryData);
-                handleRequest(queryDic, response );
-            });
-        } else {
-            var queryData = url.parse( request.url, true ).query;
-            handleRequest( queryData, response );
-        }
-    } else if( path == '/run' ) {
-        getQueryDic( request, function( queryDic ) {
-            response.writeHead(200, {'Content-type': 'text/html; charset=utf-8' });
-            response.write('hey. Response from cmd.js!');
-            var cmd = queryDic.cmd;
-            var dir = queryDic.dir;
-            var theirCheck = queryDic.check;
-            response.write('cmd: ' + cmd );
-            response.write('dir: ' + dir );
-            response.write('yourcheck: ' + theirCheck );
-            response.end();
-        });
-    } else if( path == '/run2' ) {
+//    if( path == '/' ) {
+//        if( request.method == 'POST' ) {
+//            var queryData = '';
+//            var queryDic = '';
+//            request.on('data', function(data ) {
+//                queryData += data;
+//                if( queryData.length > 1e6 ) {
+//                    queryData = '';
+//                    response.writeHead(413, {'Content-Type': 'text/plain'}).end();
+//                    request.connection.destroy();
+//                }
+//            });
+//            request.on('end', function() {
+//                queryDic = querystring.parse(queryData);
+//                handleRequest(queryDic, response );
+//            });
+//        } else {
+//            var queryData = url.parse( request.url, true ).query;
+//            handleRequest( queryData, response );
+//        }
+//    } else if( path == '/run' ) {
+//        getQueryDic( request, function( queryDic ) {
+//            response.writeHead(200, {'Content-type': 'text/html; charset=utf-8' });
+//            response.write('hey. Response from cmd.js!');
+//            var cmd = queryDic.cmd;
+//            var dir = queryDic.dir;
+//            var theirCheck = queryDic.check;
+//            response.write('cmd: ' + cmd );
+//            response.write('dir: ' + dir );
+//            response.write('yourcheck: ' + theirCheck );
+//            response.end();
+//        });
+    if( path == '/run2' ) {
         getQueryDic( request, function( queryDic ) {
             response.writeHead(200, {'Content-type': 'application/json' });
             var cmd = queryDic.cmd;
@@ -271,6 +300,7 @@ function serverFunction( request, response ) {
             cmdobj.on('exit', function(code) {
                 job.results += 'done, code: ' + code + '\n';
                 job.done = true;
+                writeJobs();
                 //response.write('done, code: ' + code );
                // response.end();
 //                job.finished = true;
@@ -288,6 +318,7 @@ function serverFunction( request, response ) {
 //                response.write( thisresult );
                 job.results += String(data);
 //                job.done = true;
+                writeJobs();
             });
             cmdobj.stderr.on('data', function(data) {
                 job.results += String(data);
@@ -342,14 +373,7 @@ function serverFunction( request, response ) {
         });
     } else if( path == '/jobs' ) {
         response.writeHead('application/json');
-        var jobssimple = [];
-        var jobIds = Object.keys( jobs );
-        for( var i = 0; i < jobIds.length; i++ ) {
-            var job = jobs[i];
-            var jobSimple = { 'id': job.id, 'cmd': job.cmd, 'args': job.args, 'done': job.done };
-            jobssimple[i] = jobSimple;
-        }
-        response.end( JSON.stringify( jobssimple ) );
+        response.end( JSON.stringify( jobsToSimplify( jobs ) ) );
         //response.end( JSON.stringify( jobs ) );
     } else if( path == '/kill' ) {
         getQueryDic( request, function( queryDic ) {
